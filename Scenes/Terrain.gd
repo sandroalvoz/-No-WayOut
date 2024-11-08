ad8b43859bad8c3e57e8276caa2ea450b0b195f7
sandroalvoz <sandroalvoza@gmail.com>
2024-11-08 13:47:32 +0100

    fix: seed update for terrain generation

--- Scenes/Terrain.gd
+++ Scenes/Terrain.gd
@@ -15,15 +15,14 @@ func generate_terrain():
 	var a_mesh:ArrayMesh
 	var surftool = SurfaceTool.new()
 	var n = FastNoiseLite.new()
+	n.seed= randi()
 	n.noise_type = FastNoiseLite.TYPE_PERLIN
-	n.frequency = 0.1
-	
-	
+	n.frequency = 0.2
 	surftool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	for z in range(zSize+1):
 		for x in range(xSize+1):
-			var y = n.get_noise_2d(x*0.5,z*0.5)*8
-			#if z==0 or z==zSize or x==0 or x==xSize: y = 0
+			var y = n.get_noise_2d(x*0.5,z*0.5)*6
+			if z==0 or z==zSize or x==0 or x==xSize: y = 0
 			var uv = Vector2()
 			uv.x =inverse_lerp(0,xSize, x)
 			uv.y =inverse_lerp(0,zSize, z)
@@ -46,8 +45,6 @@ func generate_terrain():
 	surftool.generate_normals()
 	a_mesh = surftool.commit()
 	mesh = a_mesh
-	
-	pass
 	
 func draw_sphere(pos:Vector3):
 	var ins = MeshInstance3D.new()
