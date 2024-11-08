462b395ee40e1756d8b52ef3240123f42ab00469
sandroalvoz <sandroalvoza@gmail.com>
2024-11-08 09:19:34 +0100

    feat: character movement, multiplayer test

---
+++ Scripts/MultiplayerTest.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+#https://www.youtube.com/watch?v=K62jDMLPToA
+@export var port:int 
+var peer = ENetMultiplayerPeer.new()
+@export var playerScene: PackedScene
+
+#on host pressed
+#
+
+func _add_player(id=1) :#for the host to play
+	var player =playerScene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player)
+	pass
+
+
+
+func _on_join_pressed():
+	pass # Replace with function body.
+
+
+func _on_host_pressed():
+	peer.create_server(port)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_add_player)
+	_add_player()
+	pass # Replace with function body.
--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -4,13 +4,14 @@ var tension :float = 0
 var target_velocity = Vector3.ZERO
 @export var baseSpeed: float
 @onready var soundPlayer = $AudioStreamPlayer3D
-func _ready():
-	pass # Replace with function body.
+
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed ("Attack"):
-		#perform attack
+		PerformAttack()
 		print("jaj estoy atacando")
 	pass
 	
@@ -23,22 +24,27 @@ func _physics_process(delta):
 	var direction = Vector3.ZERO
 	#direction = Input.get_vector("MoveLeft","MoveRight","MoveUp","MoveDown")
 	if Input.is_action_pressed("MoveUp"): 
-		direction.x +=1
-	if Input.is_action_pressed("MoveDown"): 
-		direction.x -=1
-	if Input.is_action_pressed("MoveLeft"): 
-		direction.z +=1
-	if Input.is_action_pressed("MoveRight"): 
 		direction.z -=1
+	if Input.is_action_pressed("MoveDown"): 
+		direction.z +=1
+	if Input.is_action_pressed("MoveLeft"): 
+		direction.x -=1
+	if Input.is_action_pressed("MoveRight"): 
+		direction.x+=1
+	print(direction)
 	if(direction!= Vector3.ZERO):
 		direction = direction.normalized()
-		$Pivot.basis = Basis.looking_at(direction)
+		self.basis = Basis.looking_at(direction)
+		#$Pivot.basis = Basis.looking_at(direction)
+	print(direction)
 	target_velocity.x = direction.x * baseSpeed *GetTension()
 	target_velocity.z = direction.z * baseSpeed *GetTension()
+	
 	#Vertical velocity
 	#if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		#target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-	
+	#if is_multiplayer_authority():
+		 #velocity = target_velocity
 	velocity = target_velocity
 	move_and_slide()
 	pass
@@ -51,4 +57,4 @@ func PerformAttack():
 	pass
 	
 func GetTension():
-	return 1.0+tension*0.2
+	return 1.0+(tension*0.2)
